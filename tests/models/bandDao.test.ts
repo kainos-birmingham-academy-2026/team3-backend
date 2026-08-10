50e6297c57c3ccfbdd0c07bd7e0124b9b82c0f8a
Mahdi-Islam071004 <mohammed.islam@kainos.com>
2026-08-10 14:56:17 +0100

    implemented mock test

--- tests/models/bandDao.test.ts
+++ tests/models/bandDao.test.ts
@@ -1,13 +1,28 @@
-import { describe, it, expect, afterEach } from 'vitest';
+import { describe, it, expect, beforeEach, vi } from 'vitest';
 import { BandDao } from '../../src/models/bandDao.ts';
 import prisma from '../../src/prismaClient.ts';
+
+vi.mock('../../src/prismaClient.ts', () => ({
+    default: {
+        band: {
+            create: vi.fn(),
+        },
+    },
+}));
+
 
 describe('BandDao - Unique Constraint', () => {
     const dao = new BandDao();
 
+    beforeEach(() => {
+        vi.clearAllMocks();
+    });
+
     it('should reject duplicate bandName', async () => {
+        vi.mocked(prisma.band.create as any).mockResolvedValueOnce({ nameId: 1, bandName: 'Mhadi', createdAt: new Date(), updatedAt: new Date() })
+        .mockRejectedValueOnce(new Error('Unique constraint failed on the fields: (`bandName`)'));
+
         await dao.create('Mhadi');
-        
         await expect(dao.create('Mhadi')).rejects.toThrow();
     });
 });
