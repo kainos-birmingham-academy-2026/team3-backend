879a459fc63c26613ab195a21551a7f78ae5b0b1
Lauren O'Brien <laurenob1@live.co.uk>
2026-08-18 19:44:39 +0100

    prisma version fixes

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,11 +8,6 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    seed: "tsx prisma/seed.ts",
   },
-  engine: "classic",
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
-
+  earlyAccess: true,
 });
