9de9400cf404e0a07f2fc4c7bc2d450e7c8348be
Lauren O'Brien <laurenob1@live.co.uk>
2026-08-26 13:43:51 +0100

    Add Key Vault module and outputs to Terraform configuration (#36)

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -10,6 +10,19 @@ module "resource_group" {
   }
 }
 
+module "key_vault" {
+  source = "../../modules/key-vault"
+
+  name                = "kv-${var.project_name}-${var.environment}"
+  location            = var.location
+  resource_group_name = module.resource_group.name
+  tags = {
+    environment = var.environment
+    managed_by  = "terraform"
+    project     = var.project_name
+  }
+}
+
 import {
   to = module.resource_group.azurerm_resource_group.this
   id = "/subscriptions/${var.subscription_id}/resourceGroups/rg-${var.project_name}-${var.environment}"
---
+++ infrastructure/modules/key-vault/main.tf
@@ -0,0 +1,14 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+
+  rbac_authorization_enabled = true
+  purge_protection_enabled   = true
+
+  tags = var.tags
+}
