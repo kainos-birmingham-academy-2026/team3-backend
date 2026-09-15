75f9822cbde642388b677062eeaa1395a3cae642
Mahdi-Islam071004 <mohammed.islam@kainos.com>
2026-09-15 22:07:22 +0100

    Now refreshes service bus, function app and comm service secret

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -250,7 +250,7 @@ resource "azurerm_key_vault_secret" "service_bus_connection_string" {
   name             = "service-bus-connection-string"
   key_vault_id     = module.key_vault.id
   value_wo         = module.notification_service_bus.backend_send_primary_connection_string
-  value_wo_version = var.application_secret_version
+  value_wo_version = var.notification_secret_version
 
   depends_on = [time_sleep.secrets_rbac_propagation]
 }
@@ -259,7 +259,7 @@ resource "azurerm_key_vault_secret" "function_service_bus_connection_string" {
   name             = "function-service-bus-connection-string"
   key_vault_id     = module.key_vault.id
   value_wo         = module.notification_service_bus.function_listen_primary_connection_string
-  value_wo_version = var.application_secret_version
+  value_wo_version = var.notification_secret_version
 
   depends_on = [time_sleep.secrets_rbac_propagation]
 }
@@ -268,7 +268,7 @@ resource "azurerm_key_vault_secret" "acs_connection_string" {
   name             = "acs-connection-string"
   key_vault_id     = module.key_vault.id
   value_wo         = module.email_communication.primary_connection_string
-  value_wo_version = var.application_secret_version
+  value_wo_version = var.notification_secret_version
 
   depends_on = [time_sleep.secrets_rbac_propagation]
 }
